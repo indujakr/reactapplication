2b0131f3cf43fec196253a542a85a2ef1aec309a
indujakr <indukrishnan16@gmail.com>
2020-09-05 13:44:51 +0530

    Updates

---
+++ precache-manifest.79fa96e87cba7801c30d0a5569b99b15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "237229c9c6b81cee5c424e68dc114732",
+    "url": "/reactapplication/index.html"
+  },
+  {
+    "revision": "c3a84723c1a9f576cd71",
+    "url": "/reactapplication/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "39fc49809cc3def3a4bb",
+    "url": "/reactapplication/static/css/main.94b1a764.chunk.css"
+  },
+  {
+    "revision": "c3a84723c1a9f576cd71",
+    "url": "/reactapplication/static/js/2.6f8a93ef.chunk.js"
+  },
+  {
+    "revision": "acb364211461e18f66aec1e8c68e542c",
+    "url": "/reactapplication/static/js/2.6f8a93ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39fc49809cc3def3a4bb",
+    "url": "/reactapplication/static/js/main.6701a2a4.chunk.js"
+  },
+  {
+    "revision": "79bd4e555d2211febc20",
+    "url": "/reactapplication/static/js/runtime-main.fb6dd984.js"
+  }
+]);
