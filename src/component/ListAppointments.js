74669da4bfdbfeca883e809e47dcbf8e70fa13bc
indujakr <indukrishnan16@gmail.com>
2020-09-05 13:35:02 +0530

    updated changes

--- src/component/ListAppointments.js
+++ src/component/ListAppointments.js
@@ -1,26 +1,43 @@
 import React, {Component} from 'react';
-
+import {FaTimes} from 'react-icons/fa';
+import Moment from "react-moment";
 class ListAppointments extends Component{
     render(){
         return (
             <div className="appointment-list item-list mb-3">
               {this.props.appointmentsList.map(item => (
-                <div className="pet-item col media py-3">
+                <div className="pet-item col media py-3" key={item.aptId}>
                   <div className="mr-3">
-                    <button className="pet-delete btn btn-sm btn-danger">X</button>
+                    <button className="pet-delete btn btn-sm btn-danger"
+                    onClick={()=>this.props.deleteAppointment(item)}><FaTimes></FaTimes></button>
                   </div>
       
                   <div className="pet-info media-body">
                     <div className="pet-head d-flex">
-                      <span className="pet-name">{item.petName}</span>
-                      <span className="apt-date ml-auto">{item.aptDate}</span>
+                      <span className="pet-name"
+                       contentEditable 
+                       suppressContentEditableWarning
+                       onBlur = { e => this.props.updateContent('petName',e.target.innerText,item.aptId)}>{item.petName}</span>
+                      <span className="apt-date ml-auto">
+                          <Moment
+                           date={item.aptDate}
+                           parse="YYYY-MM-dd hh:mm"
+                           format = "MMM-D h:mma"
+                          />
+                     </span>
                     </div>
       
                     <div className="owner-name">
                       <span className="label-item">Owner: </span>
-                      <span>{item.ownerName}</span>
+                      <span 
+                      contentEditable 
+                      suppressContentEditableWarning
+                      onBlur = { e => this.props.updateContent('ownerName',e.target.innerText,item.aptId)}>{item.ownerName}</span>
                     </div>
-                    <div className="apt-notes">{item.aptNotes}</div>
+                    <div className="apt-notes"
+                     contentEditable 
+                     suppressContentEditableWarning
+                     onBlur = { e => this.props.updateContent('aptNotes',e.target.innerText,item.aptId)}>{item.aptNotes}</div>
                   </div>
                 </div>
               ))}
